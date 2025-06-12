8ab27c7a0135c67268a295c9707a88ddbd5a4599
is4lly <isallly277@gmail.com>
2025-06-12 09:47:16 -0300

    Update saida.php

--- codigo/saida.php
+++ codigo/saida.php
@@ -1,5 +1,16 @@
 <?php
     $produto = $_GET["produto"];
     $quantidade = $_GET['quantidade'];
+
+    $idcliente = $_POST['cliente'];
+    $data = $_POST['data'];
+    $valor = $_POST["valor"];
+    $produto = $_POST['produto'] ?? [];
+    $quantidade = $_POST['quantidade'] ?? [];
+    if (empty($produto)){
+        die("Nada Selecionado");
+    }
+    $sql_venda = "INSERT INJTO td_venda (idcliente, valor_total, data) VALUES (?,?,?)";
+
     
 ?>
